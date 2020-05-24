a4978af1c62f7dca239bd43e127ac9bf65c6731f
KristofBognar <kbognar@physics.utoronto.ca>
2020-05-24 15:25:15 -0400

    added support for year selection

--- DMPs/prepDMPinput_bruker_dat.m
+++ DMPs/prepDMPinput_bruker_dat.m
@@ -1,4 +1,4 @@
-function [time,location,sza,saa,z] = prepDMPinput_bruker_dat()
+function [time,location,sza,saa,z] = prepDMPinput_bruker_dat(yr_out)
 %PREPDMPINPUT_BRUKER_DAT Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -11,28 +11,44 @@ location.altitude=610;
 
 
 %% load file with bruker measurement time info
-load('/home/kristof/work/bruker/spDB_eur_2006_2017.mat')
+load('/home/kristof/work/bruker/spDB_eur_2006_2020.mat')
 
 z=fliplr(ALTITUDE);
 
 % convert date and time to datetime (UTC time)
-tmp = strcat(data.Date, {' '}, data.Time);
+try
+    tmp = strcat(data.Date, {' '}, data.Time);
+    time_array = datetime(tmp,'InputFormat','yyyyMMdd HH:mm:ss');
+catch
+    error('Save spDB file as table, with date and time files as TEXT')
+end
 
-time_array = datetime(tmp,'InputFormat','yyyyMMdd HH:mm:ss');
+% check if only subset of years is needed
+if strcmp(yr_out,'all')
+    ind=true(size(time_array));
+else
+    if length(yr_out)==5 && yr_out(5)=='+' % input year and later
+        ind=(time_array.Year>=str2double(yr_out(1:4)));
+    elseif length(yr_out)==4 % input year only
+        ind=(time_array.Year==str2double(yr_out));
+    elseif length(yr_out)==9 && yr_out(5)=='-' % year range
+        ind=(time_array.Year>=str2double(yr_out(1:4)) & time_array.Year<=str2double(yr_out(6:9)));
+    end
+end
 
 % time output
-time.year=year(time_array);
-time.month=month(time_array);
-time.day=day(time_array);
-time.hour=hour(time_array);
-time.min=minute(time_array);
-time.sec=second(time_array);
+time.year=year(time_array(ind));
+time.month=month(time_array(ind));
+time.day=day(time_array(ind));
+time.hour=hour(time_array(ind));
+time.min=minute(time_array(ind));
+time.sec=second(time_array(ind));
 
 time.UTC=zeros(size(time.year));
 
 % solar position output
-sza=data.sza;
-saa=data.saa;
+sza=data.SZen(ind);
+saa=data.SAzm(ind);
 
 end
 
--- DMPs/sza_saa_for_LoS_meas_time.m
+++ DMPs/sza_saa_for_LoS_meas_time.m
@@ -1,8 +1,10 @@
-function [ time, location, sza_out, saa_out, z ] = sza_saa_for_LoS_meas_time( instr_name )
+function [ time, location, sza_out, saa_out, z ] = sza_saa_for_LoS_meas_time( instr_name, yr_out )
 %SZA_SAA_FOR_LOS Summary of this function goes here
 %   Detailed explanation goes here
 
 %% setup
+
+do_saoz=true;
 
 % location info
 location.latitude=80.053;
@@ -11,10 +13,10 @@ location.altitude=610;
 
 z=fliplr([0.795, [1.5:1:59.5]]);
 
-%% merge UT-GBS, PEARL-GBS, and SAOZ datasets for each species
+% data folder
+data_dir='/home/kristof/work/DMP/all_GBS_meas_times/';
 
-do_saoz=true;
-
+% file naming
 if strcmp(instr_name,'O3_VIS')        
     str_tmp='O3';        
 elseif strcmp(instr_name,'NO2_VIS')        
@@ -25,23 +27,74 @@ elseif strcmp(instr_name,'NO2_UV')
 else error('Valid trace gas inputs are O3_VIS, NO2_VIS, and NO2_UV')
 end
 
-%load GBS data and save time and solar position (use table fields,
-%location of columns is inconsistent)
-load(['/home/kristof/work/DMP/all_GBS_meas_times/UT-GBS_' str_tmp '_VCD_all_unfiltered.mat'])
-meas_all=[reanalysis.sza,reanalysis.saa,reanalysis.mjd2k]; 
+% check if only subset of years is needed
+if length(yr_out)==5 && yr_out(5)=='+' % input year and later
+    
+    do_loop=1;
+    yr_end=year(datetime(now,'convertfrom','datenum'));
+    yr_start=str2double(yr_out(1:4));
+    
+elseif length(yr_out)==9 && yr_out(5)=='-' % year range
+    
+    do_loop=1;
+    yr_end=str2double(yr_out(6:9));
+    yr_start=str2double(yr_out(1:4));
+    
+else % one year or all years
+    do_loop=0;
+end
 
-load(['/home/kristof/work/DMP/all_GBS_meas_times/PEARL-GBS_' str_tmp '_VCD_all_unfiltered.mat'])
-meas_all=[meas_all;[reanalysis.sza,reanalysis.saa,reanalysis.mjd2k]];
+%% merge UT-GBS, PEARL-GBS, and SAOZ datasets for each species
+meas_all=[];
+
+if ~do_loop % one year or all years: single file per instrument
+    
+    %load GBS data and save time and solar position (use table fields,
+    %location of columns is inconsistent)
+    try load([data_dir 'UT-GBS_' str_tmp '_VCD_' yr_out '_unfiltered.mat']), end
+    meas_all=[reanalysis.sza,reanalysis.saa,reanalysis.mjd2k]; 
+
+    try load([data_dir 'PEARL-GBS_' str_tmp '_VCD_' yr_out '_unfiltered.mat']), end
+    meas_all=[meas_all;[reanalysis.sza,reanalysis.saa,reanalysis.mjd2k]];
+
+else % multiple years
+        
+    for i=yr_start:yr_end
+        
+        try 
+            load([data_dir 'UT-GBS_' str_tmp '_VCD_' num2str(i) '_unfiltered.mat'])
+            meas_all=[meas_all;[reanalysis.sza,reanalysis.saa,reanalysis.mjd2k]]; 
+        end
+
+        try 
+            load([data_dir 'PEARL-GBS_' str_tmp '_VCD_' num2str(i) '_unfiltered.mat'])
+            meas_all=[meas_all;[reanalysis.sza,reanalysis.saa,reanalysis.mjd2k]];
+        end
+        
+    end
+    
+end
 
 % convert to N=0 for saa (SolarAzEl returns saa with N=0)
 meas_all(:,2)=meas_all(:,2)+180;
 
 if do_saoz
-    % load SAOZ data
-    load(['/home/kristof/work/SAOZ/saoz_' lower(str_tmp) '.mat'])
 
+    % load SAOZ data
+    load([data_dir 'saoz_' lower(str_tmp) '.mat'])
+
+    if strcmp(yr_out,'all')
+        ind=true(size(saoz.year));
+    else
+        if length(yr_out)==5 && yr_out(5)=='+' % input year and later
+            ind=(saoz.year>=str2double(yr_out(1:4)));
+        elseif length(yr_out)==4 % input year only
+            ind=(saoz.year==str2double(yr_out));
+        end
+    end
+    
     % add to merged table
-    meas_all=[meas_all;[saoz.sza,saoz.saa,saoz.mjd2k]];
+    meas_all=[meas_all;[saoz.sza(ind),saoz.saa(ind),saoz.mjd2k(ind)]];
 end
 
 
